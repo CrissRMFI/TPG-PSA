83da7b4dd62a75fe196d53a4bd43b72cc446dc8e
ffraga98 <ferfraga2@gmail.com>
2025-06-22 11:12:15 -0300

    add: task constructors

--- backend/src/main/java/com/psa/backend/dto/ResponseTaskDTO.java
+++ backend/src/main/java/com/psa/backend/dto/ResponseTaskDTO.java
@@ -1,10 +1,14 @@
 package com.psa.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseTaskDTO {
         Long ticketId;// 4,
         Long tareaId;// 27,
